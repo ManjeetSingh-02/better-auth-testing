341b988d1dbb77ecb2daab2de74ad2b07bc8f021
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-07-12 08:29:51 +0530

    expo added

--- server/src/core/lib/auth.ts
+++ server/src/core/lib/auth.ts
@@ -3,12 +3,26 @@ import { env } from '../config/env.js';
 import { prisma } from '../database/prisma/client.js';
 
 // external-imports
+import { expo } from '@better-auth/expo';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 
 export const auth = betterAuth({
   basePath: '/api/v1/auth',
-  trustedOrigins: env.ORIGINS,
+  trustedOrigins: [
+    ...env.ORIGINS,
+    'betterauthclientmobile://',
+
+    // Development mode - Expo's exp:// scheme with local IP ranges
+    ...(process.env.NODE_ENV === 'development'
+      ? [
+          'exp://', // Trust all Expo URLs (prefix matching)
+          'exp://**', // Trust all Expo URLs (wildcard matching)
+          'exp://192.168.*.*:*/**', // Trust 192.168.x.x IP range with any port and path
+        ]
+      : []),
+  ],
+
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
@@ -18,4 +32,5 @@ export const auth = betterAuth({
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     },
   },
+  plugins: [expo()],
 });
